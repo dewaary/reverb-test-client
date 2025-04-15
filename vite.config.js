465dfcb3d09c74f48e636a931009a4fcfad5168e
dewaari <dewa.septiadi@Ideacomindo.com>
2025-04-15 10:24:50 +0800

    setup configuration vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import fs from 'fs'
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('./certs/localhost-key.pem'),
-      cert: fs.readFileSync('./certs/localhost-cert.pem'),
-    },
-    port: 3000,
-  },
+  // Optional: Bisa tambah public base path kalau app kamu diserve dari subfolder
+  base: '/',
 })
